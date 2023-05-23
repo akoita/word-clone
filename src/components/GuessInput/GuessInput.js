a3680b7f0c51912a5f24ddf9631b1159fd3f6861
aboubakar <aboubakar@nalyze.team>
2023-05-23 02:34:13 +0200

    Exercise 1 correction

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,25 +1,36 @@
 import React from "react";
-import {GUESS_LENGTH} from "../../constants";
+import { GUESS_LENGTH } from "../../constants";
 
-function GuessInput({handleNewGuess,disabled}) {
-    const [guess, setGuess] = React.useState('')
+function GuessInput({ handleNewGuess, disabled }) {
+  const [guess, setGuess] = React.useState("");
 
-    return (<form className="guess-input-wrapper"
-                  onSubmit={event => {
-                      event.preventDefault();
-                      console.log(guess);
-                      setGuess('');
-                      handleNewGuess(guess);
-
-                  }}
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess);
+        setGuess("");
+        handleNewGuess(guess);
+      }}
     >
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input id="guess-input" type="text" value={guess} disabled={disabled} pattern={"[A-Z]{"+GUESS_LENGTH+"}"}
-               onChange={(event) => {
-            const inputValue = event.target.value.toString().toUpperCase();
-            setGuess(inputValue);
-        }}/>
-    </form>);
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        disabled={disabled}
+        required
+        minLength={GUESS_LENGTH}
+        maxLength={GUESS_LENGTH}
+        pattern={"[A-Z]{" + GUESS_LENGTH + "}"}
+        onChange={(event) => {
+          const inputValue = event.target.value.toString().toUpperCase();
+          setGuess(inputValue);
+        }}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
